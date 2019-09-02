11ae9b4a126c42bfdd73dd870c06efd825a50b5a
niamkvn <niamkvn@gmail.com>
2019-09-02 16:31:42 +0700

    initialize project

---
+++ public/javascripts/DNA.js
@@ -0,0 +1,6 @@
+module.exports = class DNA {
+  constructor() {
+    random = Math.random() * (1 - 0) + 0;
+    alert(random)
+  }
+};
---
+++ public/javascripts/sketch.js
@@ -0,0 +1,9 @@
+const DNA = require("DNA")
+
+function setup() {
+    const te  = new DNA();
+}
+
+function draw() {
+  ellipse(50, 50, 80, 80);
+}
